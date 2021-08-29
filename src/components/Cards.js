85f8b9be3085a767975690693722ff7900a43ceb
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-29 13:41:10 -0500

    updating about me

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,7 +5,8 @@ export default function Cards() {
     return (
         
         <div className="cards">
-            Charlie Aleck (they/them) is a full stack web developer and artist based in Chicago. Specializing in Javascript, React, MongoDB, Graphql and more. 
+            <p>Charlie Aleck (they/them) is a full stack web developer based in Chicago specializing in Javascript, React, MongoDB, Graphql and more.</p>
+            <p>When they aren't developing web projects, you can find them drawing and painting, tending their garden and riding their bike year-round</p>
             <img className="img" src={img}
                 alt="image of Charlie's hand holding a small bunch of purple flowers"/>
         </div>

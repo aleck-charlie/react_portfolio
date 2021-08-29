3f3acfcb3bd877bd4608c44b3b78031577d372ec
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-29 13:22:01 -0500

    adding card img and styling

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import NavBar from './components/NavBar';
 function App() {
   return (
     <div className="App">
+    
     <NavBar />
     <Cards />
     <Footer />
+
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import img from '../assets/images/flowers.jpg'
 
 export default function Cards() {
     return (
+        
         <div className="cards">
             Charlie Aleck (they/them) is a full stack web developer and artist based in Chicago. Specializing in Javascript, React, MongoDB, Graphql and more. 
+            <img className="img" src={img}
+                alt="image of Charlie's hand holding a small bunch of purple flowers"/>
         </div>
     )
 }

53f321710e67e3a94ba2d7f58a6a2e43b6886fac
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-30 10:05:21 -0500

    created pages

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,28 +1,8 @@
 import React from 'react'
-import img from '../assets/images/flowers.jpg'
 
 export default function Cards() {
     return (
-
-        <div className="cards">
-            <p>
-                Hi there! My name is Charlie Aleck (they/them). 
-                <br/>
-                <br/>
-                I'm a full stack web developer based in Chicago with a focus on Javascript, React, MongoDB, Graphql and more.
-
-                <br/>
-                <br/>
-                When I'm not working on code, you can find me delving into new cookbooks, trying new techniques in digitial illustration and tending my garden.
-                <br/>
-                <br/>
-                Feel free to browse through my Github or Resume below
-                <br/>
-                <br/>
-                <a href="https://github.com/aleck-charlie" className="link">Github</a> <a href="" className="link">Resume</a>
-            </p>
-            <img className="img" src={img}
-                alt="small bouquet of purple flowers" />
+        <div>
         </div>
     )
 }

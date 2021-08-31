1008dbcc776b46908385c32e0290549d67260684
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 12:00:37 -0500

    updated about me

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Home(){
     return (
         <div className="card">
-            <p>Hello World!</p>
+            <h1> About Charlie </h1>
+            <p>A recent graduate of Northwestern's Full Stack coding boot camp with a background in HTML, CSS, JavaScript, SQL, NoSQL (MonogoDB), jQuery, React and more. My rich life experience in commercial fishing, event coordinating, community projects, welding, art and film studies has given me the skills and drive me to seek out creative projects that are engaging, responsive and exploratory. </p>
         </div>
     );
 }

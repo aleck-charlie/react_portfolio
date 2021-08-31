76f834dc2b5ed79d9c3baa4b259d3e0f5addac05
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 16:14:55 -0500

    creating project cards

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Contact(){
     return (
         <div className="card">
-            <p>Email</p>
+            <p>Contact me at <a href="mailto:aleckcharlie@gmail.com"> aleckcharlie @ gmail</a></p>
         </div>
     );
 }
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import techblog from '../assets/imgs/techblog.png';
 
-export default function Projects(){
+export default function Projects() {
     return (
-        <div className="card">
-            <p>Projects</p>
+        <div className="projectcard">
+            <img src={techblog} alt="techblog notepad icon" className="img"/>
+                <p>Tech Blog for making and saving posts</p>
+                <div className="project-div">
+                    <a href="https://github.com/aleck-charlie/techblog" className="button"> Github</a>
+                    <br />
+                    <a href="https://techblog-ca.herokuapp.com/" className="button"> Deployed </a>
+                </div>        
         </div>
-    );
+    )
 }

1f2dc8760b974a024761fbf75d39c566460a1e57
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 16:40:44 -0500

    linked resume and linkedin

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Contact(){
     return (
         <div className="card">
-            <p>Contact me at <a href="mailto:aleckcharlie@gmail.com"> aleckcharlie @ gmail</a></p>
+            <p>Follow me on <a href="https://www.linkedin.com/in/charlie-aleck-755785205/">LinkedIn</a></p>
+            <p>Read my <a href="https://docs.google.com/document/d/1zssz-r2Og18BkP3a6k8Wp2i1cd3qWnCoMf9ByB3kINM/edit?usp=sharing">Resume</a></p>
+            <p>Reach out to me directly at <a href="mailto:aleckcharlie@gmail.com"> aleckcharlie @ gmail</a></p>
         </div>
     );
 }

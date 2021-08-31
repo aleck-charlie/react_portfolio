c7462a60e0d10244de6c36218d5e2eb993042575
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 14:37:08 -0500

    installed react-router-dom

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Router, Link } from 'react-router-dom';
 
 export default function NavBar({changePage, curPage}) {
   return (
+    <Router>
     <nav className="navbar">
       <h1> Charlie Aleck</h1>
       <div className="links">
-        <a
-          href="Projects"
+        <Link to="Projects"
           onClick={() => changePage("Projects")}
-          className={
-            curPage === "Projects" ? "nav-link active" : "nav-link"
-          }>
-            Projects
-            </a>
+          className={curPage === "Projects" ? "nav-link active" : "nav-link"
+          }/>Projects
         <a href="/">Resume</a>
         <a href="/">Contact</a>
       </div>
     </nav>
+    </Router>
   )
 }

568335225d1eff836a8903c1fbfa6ba58d80143e
Charlie Aleck <aleckcharlie@gmail.com>
2021-08-31 15:14:43 -0500

    wrapped browersrouter on nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Router, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
-export default function NavBar({changePage, curPage}) {
+export default function NavBar({ changePage, curPage }) {
   return (
-    <Router>
-    <nav className="navbar">
-      <h1> Charlie Aleck</h1>
-      <div className="links">
-        <Link to="Projects"
-          onClick={() => changePage("Projects")}
-          className={curPage === "Projects" ? "nav-link active" : "nav-link"
-          }>Projects</Link>
-          console.log(props)
-        <a href="/">Resume</a>
-        <a href="/">Contact</a>
-      </div>
-    </nav>
-    </Router>
+    <BrowserRouter>
+      <Route>
+        <nav className="navbar">
+          <h1> Charlie Aleck</h1>
+          <div className="links">
+            <Link to="Projects"
+              onClick={() => changePage("Projects")}
+              className={curPage === "Projects" ? "nav-link active" : "nav-link"
+              }>Projects</Link>
+            <a href="/">Resume</a>
+            <a href="/">Contact</a>
+          </div>
+        </nav>
+      </Route>
+    </BrowserRouter>
   )
 }
